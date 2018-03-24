61922c0efeec0db21f30e2a7ed39ac88be52d98b
Mayank Gangwar <37243315+Mayank-44@users.noreply.github.com>
2018-03-25 00:45:36 +0530

    Add files via upload
    operations on string

---
+++ string.py
@@ -0,0 +1,8 @@
+def show():
+    string1="mayank"
+    print(string1[0])
+    print(string1[1:5])
+    print(string1[:4]+" is great")
+    print(("I M %d YEARS OLD AND %s GUY")%(21,"anchal"))
+
+show();
