e2f2e2a081ddd0da9ef202052d299d3710080b74
ANCHAL MITTAL <32476930+Anchal-Mittal@users.noreply.github.com>
2018-04-02 22:16:17 +0530

    reverse a string using different approaches

--- reverse_string.py
+++ reverse_string.py
@@ -2,5 +2,8 @@ def rever():
     str="Mayank Gangwar"
     for i in range(len(str)-1,-1,-1):
         print(str[i])
-
+     str="anchal_mittal"
+     for i in reversed(range(0,len(str))):
+        print(str[i])
 rever();
+
