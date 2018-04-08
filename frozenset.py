40257dd7de9ed8ecafbf3b18f3d5380a35969828
Mayank Gangwar <37243315+Mayank-44@users.noreply.github.com>
2018-04-08 23:55:40 +0530

    Add files via upload
    use of frozenset keyword

---
+++ frozenset.py
@@ -0,0 +1,10 @@
+"""
+Frozen Sets Frozen sets are immutable objects that only support methods and 
+operators that produce a result without a?ecting the frozen set or sets to which
+they are applied.
+"""
+
+def fun():
+    frozen_set=frozenset(["aeiou","g","j"])
+    print(frozen_set)
+fun();
