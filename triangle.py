6432a39a0e54d139249b8780c7ae77714fc515fd
Mayank Gangwar <37243315+Mayank-44@users.noreply.github.com>
2018-04-09 23:38:01 +0530

    Add files via upload
    print the filled pyramid of * using for loop

---
+++ triangle.py
@@ -0,0 +1,43 @@
+def triangle():
+    for i in range(0,6):
+        for j in range(i,6):
+            print(" ",end="")
+        for k in range(1,i+1):
+            print("*",end="")
+        for l in range(1,i):
+            print("*",end="")
+        print("")
+
+triangle()
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                        
